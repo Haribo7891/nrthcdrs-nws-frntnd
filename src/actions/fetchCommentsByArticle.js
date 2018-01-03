6980c8c0474d264e10a158d6f77784453b4d425a
Haribo7891 <harry.crank@me.com>
2018-01-03 18:24:19 +0000

    [fetchCommentsByArticle] action created

---
+++ src/actions/fetchCommentsByArticle.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import * as types from './types';
+import { API_URL } from '../config/index';
+
+export const fetchCommentsByArticleRequest = (articleId) => ({
+  type: types.FETCH_COMMENTS_BY_ARTICLE_REQUEST,
+  payload: articleId
+});
+
+export const fetchCommentsByArticleSuccess = (data) => ({
+  type: types.FETCH_COMMENTS_BY_ARTICLE_SUCCESS,
+  payload: data
+});
+
+export const fetchCommentsByArticleFailure = (error) => ({
+  type: types.FETCH_COMMENTS_BY_ARTICLE_FAILURE,
+  payload: error
+});
+
+export default (articleId) => {
+  return (dispatch) => {
+    dispatch(fetchCommentsByArticleRequest(articleId));
+    return axios.get(`${ API_URL }/articles/${ articleId }/comments`)
+      .then((res) => {
+        return res.data.comments.sort((a, b) => {
+          return b.votes - a.votes;
+        });
+      })
+      .then((sortedComments) => {
+        dispatch(fetchCommentsByArticleSuccess(sortedComments));
+      })
+      // .then((res) => {
+      //   dispatch(fetchCommentsByArticleSuccess(res.data.comments));
+      // })
+      .catch((error) => {
+        dispatch(fetchCommentsByArticleFailure(error.message));
+      });
+  };
+};
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import fetchArticleById from './fetchArticleById';
 import fetchArticles from './fetchArticles';
 import fetchArticlesByTopic from './fetchArticlesByTopic';
+import fetchCommentsByArticle from './fetchCommentsByArticle';
 import fetchTopics from './fetchTopics';
 import fetchUser from './fetchUser';
 
@@ -8,6 +9,7 @@ export {
   fetchArticleById,
   fetchArticles,
   fetchArticlesByTopic,
+  fetchCommentsByArticle,
   fetchTopics,
   fetchUser
 };
