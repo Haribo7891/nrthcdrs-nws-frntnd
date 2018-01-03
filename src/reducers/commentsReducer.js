8bb04d8e0193f3ee93075a2499f3b8e9d3681599
Haribo7891 <harry.crank@me.com>
2018-01-03 18:45:56 +0000

    [DeleteComment] reducer updated

--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -26,6 +26,24 @@ export default (prevState = initialState, action) => {
       error: action.payload,
       data: {}
     });
+  case types.DELETE_COMMENT_REQUEST:
+    return Object.assign({}, prevState, {
+      loading: !prevState.loading,
+      error: null,
+      data: {}
+    });
+  case types.DELETE_COMMENT_SUCCESS:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: null,
+      data: action.payload
+    });
+  case types.DELETE_COMMENT_FAILURE:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: action.payload,
+      data: {}
+    });
   default:
     return prevState;
   }
