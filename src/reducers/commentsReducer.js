3014db70ca100a81c5edc7b6de9d345b29fa8af4
Haribo7891 <harry.crank@me.com>
2018-01-03 18:53:00 +0000

    [postComment] reducer updated

--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -44,6 +44,24 @@ export default (prevState = initialState, action) => {
       error: action.payload,
       data: {}
     });
+  case types.POST_COMMENT_REQUEST:
+    return Object.assign({}, prevState, {
+      loading: !prevState.loading,
+      error: null,
+      data: []
+    });
+  case types.POST_COMMENT_SUCCESS:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: null,
+      data: action.payload
+    });
+  case types.POST_COMMENT_FAILURE:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: action.payload,
+      data: []
+    });
   default:
     return prevState;
   }
