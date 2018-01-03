a6ef2de745b0f1da2b3fdc7230996c0bbf76aac7
Haribo7891 <harry.crank@me.com>
2018-01-03 14:12:35 +0000

    [fetchArticles] reducer created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
+import articlesReducer from './articlesReducer';
 import topicsReducer from './topicsReducer';
 import userReducer from './userReducer';
 
 const reducer = combineReducers({
+  articlesReducer,
   topicsReducer,
   userReducer
 });
