bf57c18896b363ebe78ba578a41393570c46d264
Haribo7891 <harry.crank@me.com>
2018-01-03 11:58:44 +0000

    [UserReducer] reducer created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import topicsReducer from './topicsReducer';
+import userReducer from './userReducer';
 
 const reducer = combineReducers({
-  topicsReducer
+  topicsReducer,
+  userReducer
 });
 
 export default reducer;
