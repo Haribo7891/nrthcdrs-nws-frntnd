2788fa71025b602f841b14ecddf18231891579f7
Haribo7891 <harry.crank@me.com>
2018-01-03 11:54:41 +0000

    [fetchUser] action test pass

---
+++ spec/actions/fetchUser.action.spec.js
@@ -0,0 +1,56 @@
+import { expect } from 'chai';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import fetchUser, {
+  fetchUserRequest,
+  fetchUserSuccess,
+  fetchUserFailure
+} from '../../src/actions/fetchUser';
+
+import { username } from '../utils';
+
+import { API_URL } from '../../src/config/index';
+
+const mockStore = configureMockStore([thunk]);
+
+describe('Action Creator: fetchUser', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+  it('Dispatches FETCH_USER_SUCCESS when fetching a user responds with 200 and data', () => {
+    nock(API_URL)
+      .get(`/users/${ username }`)
+      .reply(200, [ 1, 2, 3 ]);
+      
+    const expectedActions = [
+      fetchUserRequest(username),
+      fetchUserSuccess([ 1, 2, 3 ])
+    ];
+
+    const store = mockStore();
+
+    return store.dispatch(fetchUser(username))
+      .then(() => {
+        expect(store.getActions()).to.eql(expectedActions);
+      });
+  });
+  it('Dispatches FETCH_USER_FAILURE when fetching a user responds with an error', () => {
+    nock(API_URL)
+      .get(`/users/${ username }`)
+      .replyWithError({ 'message': 'error' });
+      
+    const expectedActions = [
+      fetchUserRequest(username),
+      fetchUserFailure('error')
+    ];
+
+    const store = mockStore();
+
+    return store.dispatch(fetchUser(username))
+      .then(() => {
+        expect(store.getActions()).to.eql(expectedActions);
+      });
+  });
+});
--- spec/utils.js
+++ spec/utils.js
@@ -1,1 +1,2 @@
 export const topic = 'coding';
+export const username = 'grumpy19';
