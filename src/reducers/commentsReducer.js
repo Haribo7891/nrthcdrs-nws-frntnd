26e51fa3fcbedaf0a0367466d39f1094554ba947
Haribo7891 <harry.crank@me.com>
2018-01-03 18:30:17 +0000

    [fetchCommentsByArticle] reducer created

---
+++ src/reducers/commentsReducer.js
@@ -0,0 +1,32 @@
+import * as types from '../actions/types';
+
+export const initialState = {
+  loading: false,
+  error: null,
+  data: {}
+};
+
+export default (prevState = initialState, action) => {
+  switch (action.type) {
+  case types.FETCH_COMMENTS_BY_ARTICLE_REQUEST:
+    return Object.assign({}, prevState, {
+      loading: !prevState.loading,
+      error: null,
+      data: {}
+    });
+  case types.FETCH_COMMENTS_BY_ARTICLE_SUCCESS:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: null,
+      data: action.payload
+    });
+  case types.FETCH_COMMENTS_BY_ARTICLE_FAILURE:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: action.payload,
+      data: {}
+    });
+  default:
+    return prevState;
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 
 import articlesReducer from './articlesReducer';
+import commentsReducer from './commentsReducer';
 import topicsReducer from './topicsReducer';
 import userReducer from './userReducer';
 
 const reducer = combineReducers({
   articlesReducer,
+  commentsReducer,
   topicsReducer,
   userReducer
 });
