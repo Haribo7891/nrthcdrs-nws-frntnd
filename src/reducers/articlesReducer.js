e39191ce0eaa62519c8f872ddbbb862d48ef7815
Haribo7891 <harry.crank@me.com>
2018-01-03 14:45:35 +0000

    [fetchArticlesByTopic] reducer updated

--- src/reducers/articlesReducer.js
+++ src/reducers/articlesReducer.js
@@ -26,6 +26,24 @@ export default (prevState = initialState, action) => {
       error: action.payload,
       data: {}
     });
+  case types.FETCH_ARTICLES_BY_TOPIC_REQUEST:
+    return Object.assign({}, prevState, {
+      loading: !prevState.loading,
+      error: null,
+      data: {}
+    });
+  case types.FETCH_ARTICLES_BY_TOPIC_SUCCESS:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: null,
+      data: action.payload
+    });
+  case types.FETCH_ARTICLES_BY_TOPIC_FAILURE:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: action.payload,
+      data: {}
+    });
   default:
     return prevState;
   }
