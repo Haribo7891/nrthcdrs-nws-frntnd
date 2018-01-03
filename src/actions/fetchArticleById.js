5edbcf05b9565b55bec0a31cfc51109f4138a830
Haribo7891 <harry.crank@me.com>
2018-01-03 18:15:46 +0000

    [fetchArticleById] action created

---
+++ src/actions/fetchArticleById.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import * as types from './types';
+import { API_URL } from '../config/index';
+
+export const fetchArticleByIdRequest = (articleId) => ({
+  type: types.FETCH_ARTICLE_BY_ID_REQUEST,
+  payload: articleId
+});
+
+export const fetchArticleByIdSuccess = (data) => ({
+  type: types.FETCH_ARTICLE_BY_ID_SUCCESS,
+  payload: data
+});
+
+export const fetchArticleByIdFailure = (error) => ({
+  type: types.FETCH_ARTICLE_BY_ID_FAILURE,
+  payload: error
+});
+
+export default (articleId) => {
+  return (dispatch) => {
+    dispatch(fetchArticleByIdRequest(articleId));
+    return axios.get(`${ API_URL }/articles/${ articleId }`)
+      .then(({ data: { article } }) => {
+        dispatch(fetchArticleByIdSuccess(article));
+      })
+      .catch((error) => {
+        dispatch(fetchArticleByIdFailure(error.message));
+      });
+  };
+};
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
+import fetchArticleById from './fetchArticleById';
 import fetchArticles from './fetchArticles';
 import fetchArticlesByTopic from './fetchArticlesByTopic';
 import fetchTopics from './fetchTopics';
 import fetchUser from './fetchUser';
 
 export {
+  fetchArticleById,
   fetchArticles,
   fetchArticlesByTopic,
   fetchTopics,
