a314045432bc492a5247d233bf0428fc75cb8a6c
Haribo7891 <harry.crank@me.com>
2018-01-03 18:35:27 +0000

    [deleteComment] action created

---
+++ src/actions/deleteComment.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import * as types from './types';
+import { API_URL } from '../config/index';
+
+export const deleteCommentRequest = (commentId) => ({
+  type: types.DELETE_COMMENT_REQUEST,
+  payload: commentId
+});
+
+export const deleteCommentSuccess = (data) => ({
+  type: types.DELETE_COMMENT_SUCCESS,
+  payload: data
+});
+
+export const deleteCommentFailure = (error) => ({
+  type: types.DELETE_COMMENT_FAILURE,
+  payload: error
+});
+
+export default (commentId) => {
+  return (dispatch) => {
+    dispatch(deleteCommentRequest(commentId));
+    return axios.delete(`${ API_URL }/comments/${ commentId }`)
+      .then((res) => {
+        dispatch(deleteCommentSuccess(res.data));
+      })
+      .catch((error) => {
+        dispatch(deleteCommentFailure(error.message));
+      });
+  };
+};
