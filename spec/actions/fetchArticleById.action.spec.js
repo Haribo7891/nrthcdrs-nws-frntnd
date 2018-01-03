b4de882d8387fd979d0c75aa8f3f19e8512fb9e0
Haribo7891 <harry.crank@me.com>
2018-01-03 18:18:28 +0000

    [fetchArticleById] action test pass

---
+++ spec/actions/fetchArticleById.action.spec.js
@@ -0,0 +1,56 @@
+import { expect } from 'chai';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import fetchArticleById, {
+  fetchArticleByIdRequest,
+  fetchArticleByIdSuccess,
+  fetchArticleByIdFailure
+} from '../../src/actions/fetchArticleById';
+
+import { articleId, incorrectArticleId, article } from '../utils';
+
+import { API_URL } from '../../src/config/index';
+
+const mockStore = configureMockStore([thunk]);
+
+describe('Action Creator: fetchArticleById', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+  it('Dispatches FETCH_ARTICLE_BY_ID_SUCCESS when fetching all articles responds with 200 and data', () => {
+    nock(API_URL)
+      .get(`/articles/${ articleId }`)
+      .reply(200, { article });
+      
+    const expectedActions = [
+      fetchArticleByIdRequest(articleId),
+      fetchArticleByIdSuccess(article)
+    ];
+
+    const store = mockStore();
+
+    return store.dispatch(fetchArticleById(articleId))
+      .then(() => {
+        expect(store.getActions()).to.eql(expectedActions);
+      });
+  });
+  it('Dispatches FETCH_ARTICLE_BY_ID_FAILURE when fetching all articles responds with an error', () => {
+    nock(API_URL)
+      .get(`/articles/${ incorrectArticleId }`)
+      .replyWithError({ 'message': 'error' });
+      
+    const expectedActions = [
+      fetchArticleByIdRequest(incorrectArticleId),
+      fetchArticleByIdFailure('error')
+    ];
+
+    const store = mockStore();
+
+    return store.dispatch(fetchArticleById(incorrectArticleId))
+      .then(() => {
+        expect(store.getActions()).to.eql(expectedActions);
+      });
+  });
+});
--- spec/utils.js
+++ spec/utils.js
@@ -1,2 +1,5 @@
 export const topic = 'coding';
 export const username = 'grumpy19';
+export const articleId = '5a15505e9580da3500ab4400';
+export const incorrectArticleId = 'a1b2c3a1b2c3a1b2c3';
+export const article = { article: 'Hello' };
