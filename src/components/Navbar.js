05d9f6004b0705804a8f38a83b67da58f6fc050d
Haribo7891 <harry.crank@me.com>
2018-01-15 14:43:14 +0000

    [Navbar] no longer sticky

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { Topics } from '../containers';
 
 const Navbar = () => (
-  <nav className="container card navbar-card sticky-top" style={{ backgroundColor: 'hotpink' }}>
+  <nav className="container card navbar-card navbar-color" style={{ backgroundColor: 'rgba(236, 255, 154, 0.774)' }}>
     <div className="homepage-title">
       <NavLink className="display-4" to={ '/' }>NorthCoders News</NavLink>
       <p className="lead">Filter and read the articles, then vote and comment on your favourite!</p>
