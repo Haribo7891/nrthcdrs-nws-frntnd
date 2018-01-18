c5775732f02fe1df3406de7390482d02679d000e
Haribo7891 <harry.crank@me.com>
2018-01-18 13:57:54 +0000

    [config] removed other API-URLs

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,1 @@
 export const API_URL = 'https://hwc-nrthcdrs-nws-bcknd.herokuapp.com/api';
-//export const API_URL = 'http://localhost:3000/api';
-//export const API_URL = 'https://northcoders-news-api.herokuapp.com/api';
