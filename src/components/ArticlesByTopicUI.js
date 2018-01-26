1013ef739f192dbb7e476eae2b01ce7efe547f3a
Haribo7891 <harry.crank@me.com>
2018-01-26 12:49:09 +0000

    [ArticlesByTopic] put in to rows and columns

--- src/components/ArticlesByTopicUI.js
+++ src/components/ArticlesByTopicUI.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PT from 'prop-types';
 
 const ArticlesByTopicUI = ({ article: { _id: articleId, votes, created_by: username, title } }) => (
   <div className="articles-by-topic card border-danger">
     <div className="article-list-color card-body">
-      <blockquote className="card-blockquote">
-        <div className="card-title">
-          <h5><Link to={ `/articles/${ articleId }` }>{ title }</Link></h5>
-          <p></p>
+      <div className="card-title">
+        <h6><Link to={ `/articles/${ articleId }` }>{ title }</Link></h6>
+        <p></p>
+      </div>
+      <footer className="card-footer">
+        <div className="row justify-content-between">
+          <div className="col-md-4 small">
+            <div><img src="/img/smile-o.png" width="20px" alt="smile-o"/> { votes }</div>
+          </div>
+          <div className="col-md-7 small">
+            <div><img src="/img/user.png" width="20px" alt="user"/> <Link to={ `/user/${ username }` }>{ username }</Link></div>
+          </div>
         </div>
-        <div className="footer">
-          <footer className="card-footer">
-            <span className="card-link">
-              <span><img src="/img/smile-o.png" width="20px" alt="smile-o"/> { votes }</span>
-            </span>
-            <span className="card-link">
-              <span><img src="/img/user.png" width="20px" alt="user"/> <Link to={ `/user/${ username }` }>{ username }</Link></span>
-            </span>
-          </footer>
-        </div>
-      </blockquote>
+      </footer>
     </div>
   </div>
 );
   
+ArticlesByTopicUI.propTypes = {
+  article: PT.oneOfType([ PT.object, PT.array ]).isRequired
+};
+
 export default ArticlesByTopicUI;
--- src/components/HomepageUI.js
+++ src/components/HomepageUI.js
@@ -2,43 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PT from 'prop-types';
 
-const HomepageUI = ({ articles }) => (
-  <div className="container">
-    <div className="row">
-      { Object.values(articles)
-        .map((article, i) => (
-          <div key={ i } className="col-md-4 card-group">
-            <div className="card text-center border-info wiggle2-me">
-              <div className="homepage-article-color card-body">
-                <blockquote className="card-blockquote">
-                  <div className="card-title">
-                    <h6><Link to={ `/articles/${ article._id }` }>{ article.title }</Link></h6>
-                    <p></p>
-                  </div>
-                  <div className="justify-content-between">
-                    <footer className="card-footer">
-                      <span className="card-link">
-                        <span><img src="/img/smile-o.png" width="20px" alt="smile-o"/> { article.votes }</span>
-                      </span>
-                      <span className="card-link">
-                        <span><Link to={ `/topics/${ article.belongs_to }/articles` }><img className="wiggle-me" src={ `/img/${ article.belongs_to }.png` } width="20px" alt={ `${ article.belongs_to }` }/></Link></span>
-                      </span>
-                      <span className="card-link">
-                        <span><img src="/img/user.png" width="20px" alt="user"/> <Link to={ `/user/${ article.created_by }` }>{ article.created_by }</Link></span>
-                      </span>
-                    </footer>
-                  </div>
-                </blockquote>
-              </div>
-            </div>
-          </div>
-        )) }
+const HomepageUI = ({ article: { _id: articleId, votes, created_by: username, belongs_to: topic, title } }) => (
+  <div className="card text-center border-info wiggle2-me">
+    <div className="homepage-article-color card-body">
+      <blockquote className="card-blockquote">
+        <div className="card-title">
+          <h6><Link to={ `/articles/${ articleId }` }>{ title }</Link></h6>
+          <p></p>
+        </div>
+        <div className="justify-content-between">
+          <footer className="card-footer">
+            <span className="card-link">
+              <span><img src="/img/smile-o.png" width="20px" alt="smile-o"/> { votes }</span>
+            </span>
+            <span className="card-link">
+              <span><Link to={ `/topics/${ topic }/articles` }><img className="wiggle-me" src={ `/img/${ topic }.png` } width="20px" alt={ `${ topic }` }/></Link></span>
+            </span>
+            <span className="card-link">
+              <span><img src="/img/user.png" width="20px" alt="user"/> <Link to={ `/user/${ username }` }>{ username }</Link></span>
+            </span>
+          </footer>
+        </div>
+      </blockquote>
     </div>
-  </div> 
+  </div>
 );
 
 HomepageUI.propTypes = {
-  articles: PT.oneOfType([ PT.object, PT.array ]).isRequired
+  article: PT.oneOfType([ PT.object, PT.array ]).isRequired
 };
   
 export default HomepageUI;
