28b07e65761655798fbcdce03afac3b9bac0e85a
Haribo7891 <harry.crank@me.com>
2018-01-26 13:10:44 +0000

    [Homepage] and [ArticlesByTopic] styling updated

--- src/components/ArticlesByTopicUI.js
+++ src/components/ArticlesByTopicUI.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PT from 'prop-types';
 
-const ArticlesByTopicUI = ({ article: { _id: articleId, votes, created_by: username, title } }) => (
-  <div className="articles-by-topic card border-danger">
+const ArticlesByTopicUI = ({ article: { _id: articleId, votes, created_by: username, belongs_to: topic, title } }) => (
+  <div className="card text-center border-danger wiggle2-me">
     <div className="article-list-color card-body">
       <div className="card-title">
         <h6><Link to={ `/articles/${ articleId }` }>{ title }</Link></h6>
@@ -11,11 +11,14 @@ const ArticlesByTopicUI = ({ article: { _id: articleId, votes, created_by: usern
       </div>
       <footer className="card-footer">
         <div className="row justify-content-between">
-          <div className="col-md-4 small">
-            <div><img src="/img/smile-o.png" width="20px" alt="smile-o"/> { votes }</div>
+          <div className="col small">
+            <div><Link to={ `/articles/${ articleId }` }><img src="/img/smile-o.png" width="20px" alt="smile-o"/></Link> { votes }</div>
           </div>
-          <div className="col-md-7 small">
-            <div><img src="/img/user.png" width="20px" alt="user"/> <Link to={ `/user/${ username }` }>{ username }</Link></div>
+          <div className="col small">
+            <div><Link to={ `/topics/${ topic }/articles` }><img className="wiggle-me" src={ `/img/${ topic }.png` } width="20px" alt={ `${ topic }` }/></Link></div>
+          </div>
+          <div className="col small">
+            <div><Link to={ `/user/${ username }` }><img src="/img/user.png" width="20px" alt="user"/></Link></div>
           </div>
         </div>
       </footer>
--- src/components/HomepageUI.js
+++ src/components/HomepageUI.js
@@ -5,25 +5,23 @@ import PT from 'prop-types';
 const HomepageUI = ({ article: { _id: articleId, votes, created_by: username, belongs_to: topic, title } }) => (
   <div className="card text-center border-info wiggle2-me">
     <div className="homepage-article-color card-body">
-      <blockquote className="card-blockquote">
-        <div className="card-title">
-          <h6><Link to={ `/articles/${ articleId }` }>{ title }</Link></h6>
-          <p></p>
+      <div className="card-title">
+        <h6><Link to={ `/articles/${ articleId }` }>{ title }</Link></h6>
+        <p></p>
+      </div>
+      <footer className="card-footer">
+        <div className="row justify-content-between">
+          <div className="col small">
+            <div><Link to={ `/articles/${ articleId }` }><img src="/img/smile-o.png" width="20px" alt="smile-o"/></Link> { votes }</div>
+          </div>
+          <div className="col small">
+            <div><Link to={ `/topics/${ topic }/articles` }><img className="wiggle-me" src={ `/img/${ topic }.png` } width="20px" alt={ `${ topic }` }/></Link></div>
+          </div>
+          <div className="col small">
+            <div><Link to={ `/user/${ username }` }><img src="/img/user.png" width="20px" alt="user"/></Link></div>
+          </div>
         </div>
-        <div className="justify-content-between">
-          <footer className="card-footer">
-            <span className="card-link">
-              <span><img src="/img/smile-o.png" width="20px" alt="smile-o"/> { votes }</span>
-            </span>
-            <span className="card-link">
-              <span><Link to={ `/topics/${ topic }/articles` }><img className="wiggle-me" src={ `/img/${ topic }.png` } width="20px" alt={ `${ topic }` }/></Link></span>
-            </span>
-            <span className="card-link">
-              <span><img src="/img/user.png" width="20px" alt="user"/> <Link to={ `/user/${ username }` }>{ username }</Link></span>
-            </span>
-          </footer>
-        </div>
-      </blockquote>
+      </footer>
     </div>
   </div>
 );
